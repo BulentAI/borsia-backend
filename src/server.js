9141da3e81b1794f33586f050fddc787daa86f82
BulentAI <tomdanteedgesforledges2024@gmail.com>
2026-03-15 15:51:18 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -49,7 +49,7 @@ app.use(session({
   cookie: {
     secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
-    sameSite: 'lax',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000 // 7 gün
   },
   name: 'borsia.sid'
